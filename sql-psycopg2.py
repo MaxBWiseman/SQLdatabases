6367e124135e13a359a1073fed3a9077bf51f047
Max Wiseman <maxwise70@hotmail.co.uk>
2024-08-13 18:54:49 +0000

    feat: Add SQL queries to fetch specific data from the chinook database

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -6,6 +6,30 @@ connection = psycopg2.connect(database="chinook")
 #build a cursor object of the database
 cursor = connection.cursor()
 
+#execute a query that selects all records from the Artist table
+cursor.execute('SELECT * FROM "Artist"')
+
+#select only the "Name" column from the Artist table
+cursor.execute('SELECT "Name" FROM "Artist"')
+
+#select only "Queen" from the "Artist" table
+cursor.execute('SELECT * FROM "Artist" WHERE "Name" = \'Queen\'')
+
+#select only "ArtistId" #51 from the "Artist" table
+cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = 51')
+
+#select only the albums with "ArtistId" #51 from the "Album" table
+cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = 51')
+
+#select all tracks where the composer is "Queen" from the "Track" table
+cursor.execute('SELECT * FROM "Track" WHERE "Composer" = \'Queen\'')
+#two ways to do this, this way is better for SQL injection security
+cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ('Queen',))
+
+#cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = 88')
+
+cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = 88')
+
 #fetch the results (multiple)
 results = cursor.fetchall()
 
