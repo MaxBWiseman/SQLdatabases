02d9013fa0203480fac827fac6bb5cce953c2b45
Max Wiseman <maxwise70@hotmail.co.uk>
2024-08-16 16:05:11 +0000

    feat: Add class-based models for Artist and Album tables

--- sql-orm.py
+++ sql-orm.py
@@ -8,10 +8,26 @@ from sqlalchemy.orm import sessionmaker
 db = create_engine("postgresql:///chinook")
 base = declarative_base() #this is the base class for all our models
 
+# create a class-based model for the Artist table
+class Artist(base):
+    __tablename__ = "Artist"
+
+    ArtistId = Column(Integer, primary_key=True)
+    Name = Column(String)
+    
+# create a class-based model for the Album table
+class Album(base):
+    __tablename__ = "Album"
+
+    AlbumId = Column(Integer, primary_key=True)
+    Title = Column(String)
+    ArtistId = Column(Integer, ForeignKey("Artist.ArtistId"))
+
 # instead of connecting to the database directly, we will ask for a session
 # create a new instance of sessionmaker, then point to our engine (the database)
 Session = sessionmaker(db)
 # opens an actual session by calling the Session() class defined above
 session = Session()
 
-# creating the database using 
+# creating the database using declarative_base subclass
+base.metadata.create_all(db)
